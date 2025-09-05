4b5582dc71feaad56967c9c426a2a52b44f43392
Germán Küber <german.kuber@outlook.com>
2025-09-05 20:35:27 +0100

    Improves test coverage and updates Jest config
    Expands test suite to verify rendering of key UI components, enhancing test coverage and confidence in the main interface.
    
    Updates Jest configuration to ensure compatibility with specific node modules during transformation, addressing potential test failures with certain dependencies.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders XState Builder Pattern Demo', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/XState Builder Pattern Demo/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+test('renders Toggler component', () => {
+  render(<App />);
+  const stepperElement = screen.getByText(/Estado de Debug/i);
+  expect(stepperElement).toBeInTheDocument();
+});
+
+test('renders User Form component', () => {
+  render(<App />);
+  const formElement = screen.getByText(/Formulario de Usuario/i);
+  expect(formElement).toBeInTheDocument();
 });
